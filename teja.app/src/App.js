caccc32b40007132407ad086be06f964f2e48065
iamteja9977 <thiru21ssc@gmail.com>
2022-09-27 11:30:34 +0530

    react class1

--- teja.app/src/App.js
+++ teja.app/src/App.js
@@ -1,11 +1,19 @@
-
 import './App.css';
-
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Main from './components/Main';
 function App() {
   return (
-    <h1> Hello world </h1>
-    
+    <>
+      <Header />
+      <div style={{ overflow: "auto" }}>
+        <Main />
+        <Navbar />
+      </div>
+    </>
   );
 }
 
 export default App;
+
+//Note : in-line styling in React JSx must be sent as object)key-value pair
---
+++ teja.app/src/components/Header.js
@@ -0,0 +1,15 @@
+
+function Header() {
+    return (
+        <>
+            <div style={{
+                backgroundColor: "#e5e5e5",
+                padding: "15px", textAlign: "center"
+            }}>
+                <h1>Project React</h1>
+            </div>
+        </>
+    )
+}
+
+export default Header;
